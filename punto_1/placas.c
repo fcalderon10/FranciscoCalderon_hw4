169743e84785a057c6d714794cb6a274645067bc
Francisco Calderon <francalderon867@hotmail.com>
2016-10-29 11:11:05 -0500

    Inicia punto 1

---
+++ punto_1/placas.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define L 5.0
+#define l 2.0
+#define d 1.0
+#define h 0.02
+#define v 100
+#define N 2*pow((L/h),2)
+
+int main(){
+
+  return 0;
+
+}
