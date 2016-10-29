efff0ffa797dbb65900fbfe9fefdd35191b81c3f
Francisco Calderon <francalderon867@hotmail.com>
2016-10-29 08:12:06 -0500

    Onda laboratorio

---
+++ punto_2/cuerda.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+#define n_points 1000
+#define delta_t 0.0005
+#define c 1.0
+#define iteraciones 350
+
+void solve(double *u_past , double *u_present , double *u_fut , double r);
+
+int main(){
+
+  double *x=malloc(n_points*sizeof(double));
+  double *u_init=malloc(n_points*sizeof(double));
+  double *u_present=malloc(n_points*sizeof(double));
+  double *u_fut=malloc(n_points*sizeof(double));
+  double *u_past=malloc(n_points*sizeof(double));
+  int i;
+
+  for(i=0;i<n_points;i++){
+    x[i]=(double)i/n_points;
+  }
+
+  double delta_x = x[1]-x[0];
+
+  double r=c*delta_t/delta_x;
+
+  for(i=0;i<n_points;i++){
+
+    u_init[i]=exp(-((x[i]-0.3)*(x[i]-0.3))/0.01);
+
+  }
+
+  u_init[0]=0.0;
+  u_init[n_points-1]=0.0;
+
+  u_fut[0]=0.0;
+  u_fut[n_points-1]=0.0;
+
+  for(i=1;i<n_points-1;i++){
+    u_fut[i] = u_init[i] + (r*r/2.0) * (u_init[i+1] - 2.0 * u_init[i] + u_init[i-1]);
+      }
+
+  for(i=0;i<n_points;i++){
+
+    u_past[i]=u_init[i];
+    u_present[i]=u_fut[i];
+  }
+
+  solve(u_past , u_present , u_fut, r);
+
+  return 0;
+
+}
+
+void solve(double *u_past , double *u_present , double *u_fut, double r){
+
+  int i;
+  int j;
+
+  for(j=0;j<iteraciones;j++){
+    for(i=1;i<n_points-1;i++){
+
+      u_fut[i] = (2.0*(1.0-pow(r,2)))*u_present[i] - u_past[i] + (pow(r,2))*(u_present[i+1] +  u_present[i-1]);
+    }
+    for(i=0;i<n_points;i++){
+      u_past[i] = u_present [i];
+      u_present[i] = u_fut[i];
+    }
+  }
+
+  for(i=0;i<n_points;i++){
+
+    printf("%f\n",u_present[i]);
+  }
+ 
+
+}
