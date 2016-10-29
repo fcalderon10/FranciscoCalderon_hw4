5fe9446f9c160b2f37698f13c9f11618fa4354db
Francisco Calderon <francalderon867@hotmail.com>
2016-10-29 10:34:54 -0500

    Intento 2

--- punto_2/animacion.py
+++ punto_2/animacion.py
@@ -1,4 +1,21 @@
 import numpy as np
 import matplotlib.pyplot as mp
+import os
 
 datos=np.loadtxt('cuerda.dat')
+
+for i in range(len(datos[:,0])):
+    mp.figure(i)
+    mp.plot(np.linspace(0,100,1000),datos[:,i])
+    mp.xticks(())
+    mp.yticks(())
+    mp.xlim(0,100)
+    mp.ylim(-1,1)
+    mp.savefig(str(i).zfill(3) + ".png")
+    mp.close()
+
+os.system("convert -delay 1 -loop 0 *.png cuerda.gif")
+
+mp.figure()
+mp.plot(np.linspace(0,100,1000),datos)
+mp.show()
