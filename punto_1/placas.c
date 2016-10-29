b7564a8b1382501c359cd1c1088b7f93c155ca32
Francisco Calderon <francalderon867@hotmail.com>
2016-10-29 18:12:53 -0500

    Segmentation fault (core dumped)

--- punto_1/placas.c
+++ punto_1/placas.c
@@ -6,11 +6,62 @@
 #define l 2.0
 #define d 1.0
 #define h 0.02
-#define v 100
-#define N 2*pow((L/h),2)
+#define v 100.0
+
+void em(double *V, double *Ex, double *Ey, int n, int N);
 
 int main(){
 
+  int n = (int)(L/h);
+
+  int N = 2*pow(n,2);
+
+  double *V=malloc(pow(n,2)*sizeof(double));
+  double *Ex=malloc(n*sizeof(double));
+  double *Ey=malloc(n*sizeof(double));
+
+  int i;
+
+  for(i<0;i<pow(n,2);i++){
+
+    V[i]=0.0;
+
+  }
+
+  int j;
+  
+  for(j=0;j<l;j++){
+
+    V[(int)(((n*(L-l))+L-d)/2)+j]=v/2;
+    V[(int)(((n*(L-l))+L+d)/2)+j]=v/2;
+
+  }
+
+  em(V,Ex,Ey,n,N);
+  
   return 0;
+
+}
+
+void em(double *V, double *Ex, double *Ey, int n, int N){
+
+  int i;
+  int j;
+
+  for(j=0;j<N;j++){
+
+  for(i=0;i<pow(n,2);i++){
+
+    V[i]=((V[i+(int)(n*h)]+V[i-(int)(n*h)]+V[i-(int)h]+V[i+(int)h])/4);
+
+  }
+
+ }
+
+  for(i=0;i<pow(n,2);i++){
+  
+  printf("%f",V[i]);
+  
+}
 
 }
