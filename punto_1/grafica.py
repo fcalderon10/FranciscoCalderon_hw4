f6e996554baf2002456d8efb735b6f8da07b858b
Francisco Calderon <francalderon867@hotmail.com>
2016-10-30 18:18:09 -0500

    Completo 1

--- punto_1/grafica.py
+++ punto_1/grafica.py
@@ -4,8 +4,20 @@ import matplotlib.cm as cm
 
 datos = np.loadtxt('placas.dat')
 
-datos=np.split(datos,250)
+V=datos[:,0]
+Ex=datos[:,1]
+Ey=datos[:,2]
 
-mp.figure()
-mp.imshow(datos,interpolation='bicubic')
+V=np.reshape(V,(250,250))
+Ex=np.reshape(Ex,(250,250))
+Ey=np.reshape(Ey,(250,250))
+
+x=np.linspace(0,5,250)
+y=np.linspace(0,5,250)
+
+x, y = np.meshgrid(x, y)
+
+f , em = mp.subplots(2)
+em[0].imshow(datos,interpolation='bicubic')
+em[1].streamplot(x,y,Ex,Ey)
 mp.savefig('placas.pdf')
--- punto_1/placas.c
+++ punto_1/placas.c
@@ -17,14 +17,16 @@ int main(){
   int N = 2*pow(n,2);
 
   double *V=malloc(pow(n,2)*sizeof(double));
-  double *Ex=malloc(n*sizeof(double));
-  double *Ey=malloc(n*sizeof(double));
+  double *Ex=malloc(pow(n,2)*sizeof(double));
+  double *Ey=malloc(pow(n,2)*sizeof(double));
 
   int i;
 
   for(i<0;i<pow(n,2);i++){
 
     V[i]=0.0;
+    Ex[i]=0.0;
+    Ey[i]=0.0;
 
   }
 
@@ -50,17 +52,25 @@ void em(double *V, double *Ex, double *Ey, int n, int N){
 
 for(j=0;j<N;j++){
 
-  for(i=(int)n;i<pow(n,2)-(int)n;i++){
+  for(i=n;i<pow(n,2)-n;i++){
 
-    V[i]=((V[i+(int)n]+V[i-(int)n]+V[i-1]+V[i+1])/4);
+    V[i]=((V[i+n]+V[i-n]+V[i-1]+V[i+1])/4);
+
+  }
 
   }
 
-  }
+ for(i=1;i<pow(n,2)-1;i++){
+
+   Ex[i]=(V[i-1]-V[i+1])/(2*h);
+   Ey[i]=(V[i-n]-V[i+n])/(2*h);
+   
+ }
+ 
 
   for(i=0;i<pow(n,2);i++){
   
-  printf("%f\n",V[i]);
+    printf("%f %f %f\n",V[i],Ex[i],Ey[i]);
   
 }
 
